8c54ebdf3b74bf6c4589e6177b47d091a90169c2
weiwanyan <weiwanyan@entrobus.com>
2018-01-04 18:05:34 +0800

    schedule

--- schedule/src/main/java/com/entrobus/credit/schedule/job/DemoJobDetail.java
+++ schedule/src/main/java/com/entrobus/credit/schedule/job/DemoJobDetail.java
@@ -4,6 +4,7 @@ import com.entrobus.credit.schedule.annotation.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+
 @JobDetail(jobName = "aa",groupName = "asda",cron = "0/5 * * * * ?")
 public class DemoJobDetail extends QuartzJobBean {
     @Override
