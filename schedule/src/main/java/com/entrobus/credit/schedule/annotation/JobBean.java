a6d53af7c670d4cd03d923692f76b01645eb8767
weiwanyan <weiwanyan@entrobus.com>
2018-01-05 11:35:05 +0800

    schedule

--- schedule/src/main/java/com/entrobus/credit/schedule/annotation/JobBean.java
+++ schedule/src/main/java/com/entrobus/credit/schedule/annotation/JobBean.java
@@ -7,6 +7,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ *
+ * @author laotou
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Order(1)
@@ -14,7 +18,10 @@ public @interface JobBean {
 //    @AliasFor(annotation = JobDetail.class,attribute = "jobName")
 //    String value() default "";
 //    @AliasFor(annotation = JobDetail.class,attribute = "value")
+    //任务名称
     String jobName() default "";
+    //分组名称
     String groupName() default "creditlife_default";
+    //cron 时间
     String cron() default "" ;
 }
