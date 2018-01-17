970d299f979c9691028cc5d2dbaf07321dda826f
weiwanyan <weiwanyan@entrobus.com>
2018-01-17 18:00:02 +0800

    clienLog

--- log-service/src/main/java/com/entrobus/credit/log/controller/LogController.java
+++ log-service/src/main/java/com/entrobus/credit/log/controller/LogController.java
@@ -1,13 +1,13 @@
 package com.entrobus.credit.log.controller;
 
 import com.entrobus.credit.common.util.RequestUtil;
+import com.entrobus.credit.log.service.ClientLogService;
 import com.entrobus.credit.log.service.OperationLogService;
 import com.entrobus.credit.pojo.log.ClientLog;
 import com.entrobus.credit.vo.common.CommonParameter;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +17,8 @@ import javax.servlet.http.HttpServletRequest;
 public class LogController {
     @Autowired
     private OperationLogService operationLogService;
+    @Autowired
+    private ClientLogService clientLogService;
 
     @PostMapping(value = "/garbage")
     public int clear(){
@@ -29,8 +31,8 @@ public class LogController {
         ClientLog log = new ClientLog();
         log.setAddress(RequestUtil.getIpAddr(request));
         log.setContent(form);
-//        if (StringUtils.isNotBlank(parameter.getClient()))
-//            log.setClient(parameter.getClient());
-//        log.set
+        if (StringUtils.isNotBlank(parameter.getClient()))
+            log.setClient(Integer.valueOf(parameter.getClient()));
+        clientLogService.insertSelective(log);
     }
 }
--- schedule/src/test/java/com/entrobus/credit/schedule/test/Test1.java
+++ schedule/src/test/java/com/entrobus/credit/schedule/test/Test1.java
@@ -2,6 +2,10 @@ package com.entrobus.credit.schedule.test;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Test1 {
     @Test
     public void testClass(){
@@ -9,5 +13,15 @@ public class Test1 {
         System.out.println(Test1.class.getCanonicalName());
         System.out.println(Test1.class.getSimpleName());
         System.out.println(Test1.class.getTypeName());
+    }
+    @Test
+    public void testStream(){
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(i);
+        }
+        List<Integer> list2 = list.stream().skip(900).limit(10).collect(Collectors.toList());
+        System.out.println(list);
+        System.out.println(list2);
     }
 }
