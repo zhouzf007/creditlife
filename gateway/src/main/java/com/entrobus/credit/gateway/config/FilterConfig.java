dec49dc5e5df4d9b281adb0b3dd3031be582cf44
weiwanyan <weiwanyan@entrobus.com>
2018-01-18 14:01:40 +0800

    clienLog

---
+++ gateway/src/main/java/com/entrobus/credit/gateway/config/FilterConfig.java
@@ -0,0 +1,13 @@
+package com.entrobus.credit.gateway.config;
+
+import com.entrobus.credit.gateway.filter.AccessFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
+    }
+}
